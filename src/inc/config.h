fe02cfdf956134916052bd816ce859638a8900c8
NerdyStuff <NerdyStuff@users.noreply.github.com>
2017-06-06 15:54:24 +0200

    Update config.h

--- src/inc/config.h
+++ src/inc/config.h
@@ -1,39 +1,19 @@
-#include <stdio.h>
-#include <string.h>
-#include "config.h"
+#ifndef CONFIG_H
+#define CONFIG_H
 
-typedef struct list_opt{
-	struct list_opt *nxt;
-	struct option *content;
-} confList;
+#define STD_COM_CHAR '#' //Std char for a line of comments
+#define STD_LINE_LENGTH 100 //Std lenth of lines
+#define STD_DELIMITER '=' //Std delimiter
 
-char comChar = STD_COM_CHAR;
-confList *list;
+//Structures for options
+struct option{
+	char *opt;
+	char *val;
+};
 
-void setComChar(char newComChar){
-	comChar = newComChar;
-}
 
-int readConfigFile(char *confFileName){
-	FILE *confFile = open(confFileName, "r");
-	if(!confFile){
-		printf("Could not open %s\n", confFileName);
-		return 0;
-	}
+extern void setComChar(char newComChar);
+extern int readConfigFile(char *confFileName);
+extern struct option getConfigStruct(char *optName);
 
-	//TODO Read line
-	//TODO Check if line is not a comment
-	//TODO Divide Opt and Val
-	//TODO Create new listentry and save Opt and Val
-	//TODO Repeat
-	return 1;
-}
-
-struct option getConfigStruct(char *optName){
-	confList *searchList = list;
-	if(!searchList)
-		return 0;
-	//TODO as long as optName != searchList->content->opt
-	//TODO searchList = searchList->nxt
-	//TODO return searchList->content
-}
+#endif
